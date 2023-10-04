147d9fbc604dc7732aa08eb997293442cde5367e
AnnaZubakin <146950990+AnnaZubakin@users.noreply.github.com>
2023-10-04 20:42:47 +0300

    lesson 1 as it was october 3

---
+++ Lesson2.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+//MARK: - Tuples
+
+var MyNameIs: (String, String) = ("A", "M")
+var myBossIs: (String, String, Int) = ("Jake", "Doe", 44)
+print("my boss name is \(myBossIs.0) \(myBossIs.1) and he is \(myBossIs.2) years old")
+
+//MARK: -Operators
